a6a4cfac20f3600d43231d314177891cd6e6ea5e
Jee Yi <jyi@box.com>
2016-10-18 09:42:54 -0700

    swagger2 ui added

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,40 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @ApiOperation(value = "getGreeting", nickname = "getGreeting")
+    @RequestMapping(method = RequestMethod.GET, path = "/greeting", produces = "application/json")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "World")
+    })
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success", response = Greeting.class),
+        @ApiResponse(code = 401, message = "Unauthorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "Not Found"),
+        @ApiResponse(code = 500, message = "Failure")})
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(TEMPLATE, name));
+    }
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-
 }
